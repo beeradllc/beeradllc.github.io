f2dca4934985db8ba064f7f3b91ebe16d5aed4fd
beeradllc <a.monico@yahoo.com>
2026-07-10 12:34:36 -0500

    Add Home component with welcome message
    Homepage

---
+++ Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div style={{ backgroundColor: 'black', color: 'gold', minHeight: '100vh', padding: '2rem' }}>
+    <h1>Welcome to BeeRad LLC</h1>
+    <p>We specialize in mobile heavy equipment diagnostics and repair in Lubbock, Texas.</p>
+  </div>
+);
+
+export default Home
